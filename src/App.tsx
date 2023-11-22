6ecdbd63443a882de3f1196cc1b51b18041adcff
ido elishar <idoelishar81@gmail.com>
2023-11-22 09:30:40 +0200

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // import ErrorBoundary from './component/Error'
 import MyRouter from "./component/routers/MyRouters"
-
+// import Footer from "./component/pages/UserBanner/Footer"
 
 function App() {
   return (
+    <div>
     <div className='app' style={{ justifyContent: "center", alignItems: "center" }}>
       {/* <ErrorBoundary> */}
       <MyRouter />
       {/* </ErrorBoundary> */}
+    </div>
     </div>
   )
 }
