c2e38081892a5f164249c81231cdccee037fab6d
moshe lapidot <141305314+moshelapi@users.noreply.github.com>
2023-11-26 11:45:43 +0200

    MUI HEDER & FOOTER

--- src/component/pages/AllProduct/AllProduct.tsx
+++ src/component/pages/AllProduct/AllProduct.tsx
@@ -2,35 +2,49 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Product } from "../../interface";
 import ProductCard from "../../mui/CardProduct";
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { useNavigate } from "react-router";
 const store = import.meta.env.VITE_STORE_SERVER;
 
+export default function AllProduct() {
+  const Navigate = useNavigate();
+  const [data, setData] = useState<Product[]>([]);
 
-export default function  AllProduct(){
-
-    const Navigate = useNavigate()
-    const [data, setData] = useState<Product[]>([]);
-
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(
-          `  ${store}/api/products`
-          );
-          setData(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      fetchData();
-    }, []);
-    return (
-        <Stack sx={{display:'flex',flexWrap:'wrap',flexDirection:'row',justifyContent:'center'}}>
-          <Typography variant="h3" > select product</Typography>
-          {data.map((product: Product) => (
-            <ProductCard onClick={()=>{Navigate(`/addBanner/${product.id}`)}} key={product.id} product={product} />
-          ))}
-        </Stack>
-      );
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`  ${store}/api/products`);
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <Box>
+      <Typography sx={{ textAlign: "center", margin: "30px" }} variant="h3">
+        {" "}
+        select product
+      </Typography>
+      <Stack
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        {data.map((product: Product) => (
+          <ProductCard
+            onClick={() => {
+              Navigate(`/addBanner/${product.id}`);
+            }}
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </Stack>
+    </Box>
+  );
 }
